41dac4327c6cfcf76f6b23b18772e4e2587bfafb
Miralas <miralas777@yandex.ru>
2015-09-10 02:46:16 +0300

    Fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907170125) do
+ActiveRecord::Schema.define(version: 20150909230727) do
 
   create_table "banner_regions", force: :cascade do |t|
     t.string   "width"
@@ -97,6 +97,12 @@ ActiveRecord::Schema.define(version: 20150907170125) do
   end
 
   add_index "company_reviews", ["user_id"], name: "index_company_reviews_on_user_id"
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "events", force: :cascade do |t|
     t.integer  "author_id"
