2439a4902d47acbe5a22ba8d332c55c7237dd4e1
Miralas <miralas777@yandex.ru>
2015-09-19 15:06:47 +0300

    fix migration

--- db/migrate/20150802142828_create_vacancy_answers.rb
+++ db/migrate/20150802142828_create_vacancy_answers.rb
@@ -2,7 +2,7 @@ class CreateVacancyAnswers < ActiveRecord::Migration
   def change
     create_table :vacancy_answers do |t|
       t.references :resume, index: true, foreign_key: true
-      t.references :company_vacancy, index: true, foreign_key: true
+      t.references :vacancy, index: true, foreign_key: true
       t.datetime :when_viewed
 
       t.timestamps null: false
