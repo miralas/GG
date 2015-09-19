2a32ef59f2904d3ea478741c1eaab7283da1e5fa
Miralas <miralas777@yandex.ru>
2015-09-19 15:09:16 +0300

    remove error

--- app/models/vacancy_answer.rb
+++
@@ -1,4 +0,0 @@
-class VacancyAnswer < ActiveRecord::Base
-  belongs_to :resume
-  belongs_to :company_vacancy
-end
--- spec/factories/vacancy_answers.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :vacancy_answer do
-    resume nil
-company_vacancy nil
-when_viewed "2015-08-02 17:28:28"
-  end
-
-end
